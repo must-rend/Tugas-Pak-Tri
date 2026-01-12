7fa34cbc6139ae8ee315decf11c753f2e5a61052
must-rend <olehmang1234@gmail.com>
2026-01-12 09:46:12 +0700

    Add files via upload

---
+++ belajarjava/BelajarJava.java
@@ -0,0 +1,46 @@
+
+package com.mycompany.belajarjava;
+
+public class BelajarJava {
+
+    public static void main(String[] args) {
+        int angka;
+        angka=10;
+        
+        double tinggi;
+        tinggi=170.5; 
+        
+        float berat;
+        berat=55.3f;
+        
+        char ranking;
+        ranking= 'A';
+        
+        boolean lulus;
+        lulus= true;
+        
+        long jarak;
+        jarak=150000000L;
+        
+        short nilai;
+        nilai=120;
+        
+        byte bit;
+        bit=10;
+       
+        
+        System.out.println(angka);
+        System.out.println("Angka yang anda inputkan adalah:" +angka);
+        angka=100;
+        System.out.println("Angka terbaru adalah: " +angka);
+        System.out.println("Tinggi Badan Rendi: " +tinggi);
+        System.out.println("Berat Badan Rendi: " +berat);
+        System.out.println("Rangking rendi adalah: " +ranking);
+        System.out.println("Apakah  Rendi Lulus: " +lulus);
+        System.out.println("Jarak Rumah Rendi Ke Sekolah: " +jarak);
+        System.out.println("Total Nilai Rendi: " +nilai);
+        System.out.println("Berapa Bit: " +bit);
+        
+
+    }
+}
